155629dc0b749041e1ef0222299770420bbc6c83
bhuvan <56767330+bhuvan2205@users.noreply.github.com>
2022-10-26 18:35:51 +0530

    adding fallback page

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -1,6 +1,7 @@
 import { createClient } from 'contentful';
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 import Image from 'next/image';
+import Skeleton from '../../components/Skeleton';
 
 const client = createClient({
   space: process.env.CONTENTFUL_SPACE_ID,
@@ -17,7 +18,7 @@ export const getStaticPaths = async () => {
   })
   return {
     paths,
-    fallback: false
+    fallback: true
   }
 }
 export async function getStaticProps({ params }) {
@@ -26,14 +27,21 @@ export async function getStaticProps({ params }) {
 
   return {
     props: { recipe: items[0] },
-    revalidate: 10
+    revalidate: 2
   }
 }
 
 
 const RecipeDetails = ({ recipe }) => {
+
+  if (!recipe) {
+
+    return <Skeleton />;
+  }
+
   console.log(recipe);
   const { featuredImage, title, cookingTime, ingredients, method } = recipe.fields;
+
   return (
     <div>
       <div className='banner'>
